6f584746c191800ae6f11f43c4a0fe581e94859c
Youness El Andaloussi <youness@elandaloussi.net>
2025-06-25 08:02:51 +0100

    Added hour and time to log

--- ApacheLog/log_config.php
+++ ApacheLog/log_config.php
@@ -4,27 +4,32 @@ $config = [
     "paths"=> [
         "basedir" => $basedir,
         "log" => "$basedir/logs",
-        "engines" => "$basedir/engines",
+        "engine_file" => "$basedir/config/engines.yaml", // Update to YAML file
         "customgraphs" => "$basedir/customgraphs"
     ],
     "columns" => [
-        "date" => "Date",
-        "ip" => "IP Address",
-        "hostname" => "Hostname",
-        "path" => "Path",
-        "referer" => "Referer",
-        "agent" => "User Agent"
+        ["date", "Date"],
+        ["ip", "IP Address"],
+        ["hostname", "Hostname"],
+        ["path", "Path"],
+        ["referer", "Referer"],
+        ["agent", "User Agent"]
     ],
-    "file" => "visitors.txt",
+    "defaultfile" => "access.log",
     "logfolder" => "$basedir/logs",
-    "engines"=>"engines.txt",
-    "customgraphs" => array(
-        "ip" => "IP Address",
-        "path" => "Path",
-        "agent" => "User Agent",
-        "hostname" => "Hostname",
-        "referer" => "Referer"
-    )
+    "customgraphs" => [
+         "path" => ["field" => "path", "label" => "Path"],
+         "agent" => ["field" => "agent", "label" => "User Agent"],
+         "hostname" => ["field" => "hostname", "label" => "Hostname"],
+         "referer" => ["field" => "referer", "label" => "Referer"],
+         "vhost"=> ["field" => "vhost", "label" => "Virtual Host"],
+         "verb" => ["field" => "verb", "label" => "Method"],
+         "ip" => ["field" => "ip", "label" => "IP Address"],
+         "day"=> ["field" => "day", "label" => "Date"],
+         "hour"=> ["field" => "hour", "label" => "Hour"]
+    ]
 ];
 
 
+$config["columns"]=array(["date"],["ip"],["hostname"],["path"],["referer"],["agent"],["host"]);
+$config["columns"]=array(0=>["date"],6=>["host"],3=>["path"],2=>["hostname"],4=>["referer"],1=>["ip"],5=>["agent"]);
